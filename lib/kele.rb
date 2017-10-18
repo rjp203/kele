e5fe5f4163c8fa2bdaf4a03c3e4b94b3f105b0c2
Ryan Popovic <rjp203@hotmail.com>
2017-10-18 02:11:31 +0000

    User Story 4 Mentor Availability: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -3,17 +3,28 @@ require 'json'
 
 class Kele
   include HTTParty
-  base_uri 'https://www.bloc.io/api/v1'
-  
-  def initialize(username, password)
-    response = self.class.post('/sessions', body: { username: "#{username}", password: "#{password}" } )
+  base_uri "https://www.bloc.io/api/v1/"
+
+  def initialize(email, password)
+    response = self.class.post(api_url + 'sessions', body: { email: "#{email}", password: "#{password}" })
     @auth_token = response["auth_token"]
     raise "ERROR: Username or password is invalid." if @auth_token.nil?
   end
   
   def get_me
-    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url + 'users/me', headers: { "authorization" => @auth_token })
     body = JSON.parse(response.body)
+  end
+  
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url + '/mentors/' + '#{mentor_id}' + '/student_availability', headers: {"authorization" => @auth_token })
+    body = JSON.parse(response.body)
+  end
+  
+  private
+  
+  def api_url
+    "https://www.bloc.io/api/v1/"
   end
   
 end
