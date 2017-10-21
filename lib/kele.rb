d37bbc8d574671f07f38802894fbb01e8727a99f
Ryan Popovic <rjp203@hotmail.com>
2017-10-21 22:03:48 +0000

    User Story 7 Checkpoint Submission: Revision 1

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,37 +8,37 @@ class Kele
   base_uri "https://www.bloc.io/api/v1/"
 
   def initialize(email, password)
-    response = self.class.post(api_url + 'sessions', body: { email: '#{email}', "password": "#{password}" })
+    response = self.class.post(api_url + "sessions", body: { "email": "#{email}", "password": "#{password}" })
     @auth_token = response["auth_token"]
     raise "ERROR: Username or password is invalid." if @auth_token.nil?
   end
   
   def get_me
-    response = self.class.get(api_url + 'users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url + "users/me", headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
   end
   
   def get_mentor_availability(mentor_id)
-    response = self.class.get(api_url + 'mentors/#{mentor_id}/student_availability', headers: {"authorization" => @auth_token })
+    response = self.class.get(api_url + "mentors/#{mentor_id}/student_availability", headers: {"authorization" => @auth_token })
     @mentor = JSON.parse(response.body)
   end
 
   def get_messages(page)
     if page == nil
-      response = self.class.get(api_url + 'message_threads', headers: { "authorization" => @auth_token })
+      response = self.class.get(api_url + "message_threads", headers: { "authorization" => @auth_token })
     else
-      response = self.class.get(api_url + 'message_threads?page=' + '#{page}', headers: { "authorization" => @auth_token })
+      response = self.class.get(api_url + "message_threads?page=" + "#{page}", headers: { "authorization" => @auth_token })
     end
     @messages = JSON.parse(response.body)
   end
 
   def create_message(sender, recipient_id, token, subject, stripped_text)
-    response = self.class.post(api_url + 'messages', body: { sender: "#{sender}", recipient_id: "#{recipient_id}", token: "#{token}", subject: "#{subject}", stripped_text: "#{stripped_text}" }, headers: { "authorization" => @auth_token, "content_type" => application/json })
+    response = self.class.post(api_url + "messages", body: { "sender": "#{sender}", "recipient_id": "#{recipient_id}", "token": "#{token}", "subject": "#{subject}", "stripped_text": "#{stripped_text}" }, headers: { "authorization" => @auth_token, "content_type" => application/json })
     @message = JSON.parse(response.body)
   end
   
   def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)    
-    response = self.class.post(api_url + 'checkpoint_submissions', body: { checkpoint_id: "#{checkpoint_id}", assignment_branch: "#{assignment_branch}", assignment_commit_link: "#{assignment_commit_link}", comment: "#{comment}", enrollment_id: "#{enrollment_id}" }, headers: { "authorization" => @auth_token, "content_type" => application/json })
+    response = self.class.post(api_url + "checkpoint_submissions", body: { "checkpoint_id": "#{checkpoint_id}", "assignment_branch": "#{assignment_branch}", "assignment_commit_link": "#{assignment_commit_link}", "comment": "#{comment}", "enrollment_id": "#{enrollment_id}" }, headers: { "authorization" => @auth_token, "content_type" => application/json })
     @submission = JSON.parse(response.body)
   end
  
